8190117b1a9786a66de745c70b912abc003d7a13
Fran <francielepa03@gmail.com>
2025-06-09 22:22:21 -0300

    feito

--- listadenoticias.php
+++ listadenoticias.php
@@ -3,85 +3,73 @@ include "cabecalho.php";
 ?>
 <h2>Todas as notícias</h2>
 <div class="row">
+<div class="container">
+  <h2 class="text-center my-4">Todas as notícias</h2>
+  <div class="row justify-content-center">
 
-    <div class="col-3 mb-4">
-        <div class="card" style="width: 18rem;">
-            <a href="umanoticia.php">
-                <img src="img/noticia1.png" class="card-img-top">
-            </a>
-            <div class="card-body">
-                <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
-            </div>
+    <div class="col-md-4 mb-4">
+      <div class="card">
+        <a href="umanoticia.php">
+          <img src="img/noticia1.png" class="card-img-top">
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
         </div>
+      </div>
     </div>
 
-    <div class="col-3 mb-4">
-        <div class="card" style="width: 18rem;">
-            <a href="umanoticia.php">
-                <img src="img/noticia1.png" class="card-img-top">
-            </a>
-            <div class="card-body">
-                <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
-            </div>
+    <div class="col-md-4 mb-4">
+      <div class="card">
+        <a href="umanoticia.php">
+          <img src="img/noticia1.png" class="card-img-top">
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
         </div>
+      </div>
     </div>
 
-    <div class="col-3 mb-4">
-        <div class="card" style="width: 18rem;">
-            <a href="umanoticia.php">
-                <img src="img/noticia1.png" class="card-img-top">
-            </a>
-            <div class="card-body">
-                <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
-            </div>
+    <div class="col-md-4 mb-4">
+      <div class="card">
+        <a href="umanoticia.php">
+          <img src="img/noticia1.png" class="card-img-top">
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
         </div>
+      </div>
     </div>
 
-    <div class="col-3 mb-4">
-        <div class="card" style="width: 18rem;">
-            <img src="img/noticia1.png" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
-            </div>
+    <div class="col-md-4 mb-4">
+      <div class="card">
+        <img src="img/noticia1.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
         </div>
+      </div>
     </div>
 
-    <div class="col-3 mb-4">
-        <div class="card" style="width: 18rem;">
-            <img src="img/noticia1.png" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
-            </div>
+    <div class="col-md-4 mb-4">
+      <div class="card">
+        <img src="img/noticia1.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
         </div>
+      </div>
     </div>
 
-    <div class="col-3 mb-4">
-        <div class="card" style="width: 18rem;">
-            <img src="img/noticia1.png" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
-            </div>
+    <div class="col-md-4 mb-4">
+      <div class="card">
+        <img src="img/noticia1.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
         </div>
+      </div>
     </div>
 
-    <div class="col-3 mb-4">
-        <div class="card" style="width: 18rem;">
-            <img src="img/noticia1.png" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-3 mb-4">
-        <div class="card" style="width: 18rem;">
-            <img src="img/noticia1.png" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Quarteto Fantástico | Diretor revela quadrinhos que inspiraram o novo filme</h5>
-            </div>
-        </div>
-    </div>
-    
+   
+  </div>
 </div>
+
 
 <?php include "rodape.php"; ?>
